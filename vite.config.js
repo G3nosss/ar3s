e2693a6e0165bbef62f67fbd269ff24b113b2362
Aman Sarma <aman@pragjyotishcollege.ac.in>
2026-01-11 14:40:33 +0530

    Fixed Vite Config for Buffer

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,13 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 export default defineConfig({
   plugins: [
     nodePolyfills({
+      // This forces the browser to have the missing tools
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
       protocolImports: true,
     }),
   ],
-  define: {
-    global: 'window', 
-  },
 });
